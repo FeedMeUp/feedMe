b4194452bde89074771509b855444c0f44495f48
Apprentice <apprentice@StMac0-100.local>
2016-11-11 14:59:50 +1300

    styles form page

--- hyperscript/food.js
+++ hyperscript/food.js
@@ -3,14 +3,14 @@ var h = require('hyperscript')
 module.exports =
 function (back, getFood) {
   return h('div#form', [
-    h('button', {type: 'submit', onclick: back}, 'back'),
-    h('form', [
+    h('button', {onclick: back}, 'back'),
+    h('div', [
       h('input', {type: 'text', placeholder: 'FOOD'}),
       h('input', {type: 'text', placeholder: 'Item'}),
       h('input', {type: 'text', placeholder: 'Quantity'}),
       h('input', {type: 'text', placeholder: 'Date'}),
       h('input', {type: 'text', placeholder: 'Pick Up Address'}),
-      h('button#submit', {type: 'submit', onclick: getFood()}, 'Submit')
+      h('button', {onclick: getFood}, 'Submit')
     ])
   ])
 }
--- hyperscript/form.js
+++ hyperscript/form.js
@@ -3,7 +3,6 @@ var h = require('hyperscript')
 module.exports =
 function (back) {
   return h('div#form', [
-    h('button', {type: 'submit', onclick: back}, 'back'),
     h('form', [
       h('input', {type: 'text', name: 'companyName', placeholder: 'Company Name'}),
       h('input', {type: 'text', name: 'logo', placeholder: 'Logo link'}),
@@ -11,7 +10,8 @@ function (back) {
       h('input', {type: 'text', name: 'quantity', placeholder: 'Quantity'}),
       h('input', {type: 'text', name: 'pickUpBy', placeholder: 'Pick Up By'}),
       h('input', {type: 'text', name: 'address', placeholder: 'Pick Up Address'}),
-      h('button#submit', {type: 'submit'}, 'Submit')
+      h('button.formButton', {type: 'submit'}, 'Submit'),
+      h('button.formButton', {type: 'submit', onclick: back}, 'Back'),
     ])
   ])
 }
