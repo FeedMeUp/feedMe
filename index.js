fbd02d11f44c225076f0cd0889c0c633287ce039
rodrigo-delgado <rod.delgado85@gmail.com>
2016-11-11 14:51:33 +1300

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 var form = require('./hyperscript/form')
+var food = require('./hyperscript/food')
 var homepage = require('./hyperscript/homepage')
+var morphdom = require('morphdom')
+var h = require('hyperscript')
+var request = require('superagent')
 
 var main = document.querySelector('main')
+var home = homepage(goToForm, goToFood)
 
-main.appendChild(homepage)
-main.appendChild(form)
+function updatePage(newPage) {
+    morphdom(main, h('main', {}, newPage))
+}
+
+updatePage(home)
+
+function goToForm(){
+  updatePage(form(goToHomepage))
+}
+function goToFood(){
+  request
+    .get('https://feedmeapi.herokuapp.com/api/v1/food')
+    .end(function(err, res){
+      console.log(res.body);
+      updatePage(food(goToHomepage, res.body))
+    })
+}
+function goToHomepage(){
+  updatePage(home)
+}
+// function getFood(){
+//   // e.preventDefault()
+//   request
+//     .get('https://feedmeapi.herokuapp.com/api/v1/food')
+//     .end(function(err, res){
+//       console.log(res.body);
+//     })
+// }
