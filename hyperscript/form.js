3853e52834d453145c3976b96126a6e1689d6750
Apprentice <apprentice@StMac0-100.local>
2016-11-11 14:13:31 +1300

    adds initial styles

--- hyperscript/form.js
+++ hyperscript/form.js
@@ -1,22 +1,17 @@
-var h = require("hyperscript")
+var h = require('hyperscript')
 
 module.exports =
-function (pageContent) {
+function (back) {
   return h('div#form', [
+    h('button', {type: 'submit', onclick: back}, 'back'),
     h('form', [
-      h('input', {type: "text", placeholder: "Business Name"}),
-      h('input', {type: "text", placeholder: "Item"}),
-      h('input', {type: "text", placeholder: "Quantity"}),
-      h('input', {type: "text", placeholder: "Date"}),
-      h('input', {type: "text", placeholder: "Pick Up Address"}),
-      h('button#submit', {
-        value: "submit",
-        onclick: ev => {
-          ev.preventDefault()
-          console.log("I was clicked!!!")
-          // pageContent()
-        },
-      }, "Submit")
+      h('input', {type: 'text', name: 'companyName', placeholder: 'Company Name'}),
+      h('input', {type: 'text', name: 'logo', placeholder: 'Logo link'}),
+      h('input', {type: 'text', name: 'item', placeholder: 'Item'}),
+      h('input', {type: 'text', name: 'quantity', placeholder: 'Quantity'}),
+      h('input', {type: 'text', name: 'pickUpBy', placeholder: 'Pick Up By'}),
+      h('input', {type: 'text', name: 'address', placeholder: 'Pick Up Address'}),
+      h('button#submit', {type: 'submit'}, 'Submit')
     ])
   ])
 }
--- hyperscript/unclaimedItems.js
+++
@@ -1,8 +0,0 @@
-var h = require("hyperscript")
-
-module.exports =
-h('div#form',
-  h('form',
-    [h('input', {type: "text", placeholder: "Business Name"}),
-    h('input', {type: "text", placeholder: "Item"}),
-    h
